f274f749b37d647566da47491a37097a3821d1cf
gumballipa <henriquedejesusdossantos702@gmail.com>
2025-08-19 21:18:24 -0300

    aula9v2

--- aula09/aula09-atividade02.py
+++ aula09/aula09-atividade02.py
@@ -1,10 +1,12 @@
 produtos = (
-    ("arroz", 5.99)
-    ("feijão", 6.99)
-    ("macarrão", 7.99)
-    ("açúcar", 4.99)
-    ("sal", 2.99)
+    ("arroz", 5.99),
+    ("feijão", 6.99),
+    ("macarrão", 7.99),
+    ("açúcar", 4.99),
+    ("sal", 2.99),
 )
+
 print("Produtos Disponiveis")
+
 for nome, preco in produtos:
     print(f"{nome:.<20}: R$ {preco:.2f}")
